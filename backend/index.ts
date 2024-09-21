4e8f8be44db237a9e3a645af6421ca9c20b48184
Oybek Sultanov <oibekzzz09@gmail.com>
2024-09-21 15:53:31 +0600

    Реализация моделей Item и Category для реализаций нашей серверной части.

--- backend/index.ts
+++ backend/index.ts
@@ -2,13 +2,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 import config from './config';
 import usersRouter from './routes/users';
+import categoriesRouter from './routes/category';
+import itemsRouter from './routes/items';
+import cors from 'cors';
 
 const app = express();
 const port = 8000;
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cors());
 app.use('/users', usersRouter)
+app.use('/categories', categoriesRouter);
+app.use('/items', itemsRouter);
 
 const run = async () => {
   await mongoose.connect(config.database);
