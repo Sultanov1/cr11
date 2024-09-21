c8427ec4333504517448348940f502a5c0d1090f
Oybek Sultanov <oibekzzz09@gmail.com>
2024-09-21 15:24:06 +0600

    Добавление недостающих зависимостей также реализация авторизации пользователя.

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
+import usersRouter from './routes/users';
 
 const app = express();
 const port = 8000;
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use('/users', usersRouter)
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);;
-});
+const run = async () => {
+  await mongoose.connect(config.database);
+
+  app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  })
+};
+
+run().catch(console.error);
+
---
+++ backend/routes/users.ts
@@ -0,0 +1,71 @@
+import express from 'express';
+import User from '../models/User';
+import mongoose from 'mongoose';
+
+const usersRouter = express.Router();
+
+usersRouter.post('/', async (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+      displayName: req.body.displayName,
+      phone: req.body.phone,
+    });
+
+    user.generateToken();
+
+    await user.save();
+    return res.send({ message: 'user  registered!', user });
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(error);
+    }
+    return next(error);
+  }
+});
+
+usersRouter.post('/sessions', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) {
+      return res.status(400).send({ error: 'Username or Password not found!' });
+    }
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if (!isMatch) {
+      return res.status(400).send({ error: 'Username or Password not found!' });
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return res.send({ message: 'User login!', user });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+usersRouter.delete('/sessions', async (req, res, next) => {
+  try {
+    const token = req.get('Authorization');
+    const success = {message: 'Success'};
+
+    if (!token) return res.send(success);
+
+    const user = await User.findOne({token});
+
+    if (!user) return res.send(success);
+
+    user.generateToken();
+    user.save();
+
+    return res.send(success);
+  } catch (e) {
+    return next(e);
+  }
+});
+
+export default usersRouter;
